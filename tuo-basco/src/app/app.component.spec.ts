f3f8fe6b011f522a94ce92584d2212e85ab47f28
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2022-09-27 18:28:46 +0000

    punto1

--- tuo-basco/src/app/app.component.spec.ts
+++ tuo-basco/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tuo-Basco'`, () => {
+  it(`should have as title 'tuo-basco'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tuo-Basco');
+    expect(app.title).toEqual('tuo-basco');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tuo-Basco app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tuo-basco app is running!');
   });
 });
