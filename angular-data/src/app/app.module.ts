a1f16f4c4bdc2c54a1499b672708fd582f3acc19
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2022-10-06 09:49:34 +0000

    punto1

--- angular-data/src/app/app.module.ts
+++ angular-data/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserItemComponent,
     HelloWorldComponent,
-    UserListComponent
+    UserListComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
