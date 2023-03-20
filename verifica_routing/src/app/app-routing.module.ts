7919cbfb219604796da19257dd7a101981285bb2
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2023-03-20 08:27:21 +0000

    punto2a/b

--- verifica_routing/src/app/app-routing.module.ts
+++ verifica_routing/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemBirreComponent } from './item-birre/item-birre.component';
+import { SearchBirreComponent } from './search-birre/search-birre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/search', pathMatch: 'full' },
+  { path: 'search', component: SearchBirreComponent },
+  { path: 'item/:id', component: ItemBirreComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- verifica_routing/src/app/app.module.ts
+++ verifica_routing/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBirreComponent } from './search-birre/search-birre.component';
+import { ItemBirreComponent } from './item-birre/item-birre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBirreComponent,
+    ItemBirreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
