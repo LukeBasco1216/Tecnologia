99089e4e3d31ebfac244b37ad87b7904a4e61ba0
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2023-04-03 09:27:02 +0000

    in

---
+++ angular-maps/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'googlemaps';
+  //Aggiungiamo latitudine e longitudine di un luogo
+  center : any;
+  position : any;
+  label : string;
+  constructor()
+  {
+    this.center={lat: 45.506738, lng: 9.190766};
+    this.position = this.center;
+    this.label = "ciao";
+  }
+}
