8181561466696e359c7c6b74b9d94178727a4dd6
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2023-04-13 12:57:50 +0000

    inizio file 01.1

--- angular-maps/src/app/app.component.ts
+++ angular-maps/src/app/app.component.ts
@@ -10,17 +10,35 @@ export class AppComponent {
   //Aggiungiamo latitudine e longitudine di un luogo
   center : any;
   position : any;
+  circleOptions : any;
   label : string;
 
 // può essere fatto in questo modo fuori dal constructor
   coordC={lat: 45.516513, lng: 9.159780};
   nome = "casa";
+  colour = {fillColor : 'red'}
 
   constructor()
   {
     this.center={lat: 45.506738, lng: 9.190766};
     this.position = this.center;
     this.label = "ciao";
+    this.circleOptions = this.colour
+  }
+  
+  red() : void
+  {
+    this.circleOptions = {fillColor : 'red'}
+  }
+
+  yellow() : void
+  {
+    this.circleOptions = {fillColor : 'yellow'}
+  }
+
+  green() : void
+  {
+    this.circleOptions = {fillColor : 'green'}
   }
 
 }
