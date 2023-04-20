92e824298d3c7244edb138b2a548c4d9579dcbae
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2023-04-20 12:59:47 +0000

    esercizi 3 animali file 01.1

--- angular-maps/src/app/app.component.ts
+++ angular-maps/src/app/app.component.ts
@@ -12,11 +12,20 @@ export class AppComponent {
   position : any;
   circleOptions : any;
   label : string;
+  markerOptions: google.maps.MarkerOptions;
+
+  markerOptionsbear: google.maps.MarkerOptions;
+  markerOptionselephant: google.maps.MarkerOptions;
+  markerOptionscow: google.maps.MarkerOptions;
 
 // può essere fatto in questo modo fuori dal constructor
   coordC={lat: 45.516513, lng: 9.159780};
   nome = "casa";
   colour = {fillColor : 'red'}
+
+  coordbear={lat: 45.507525, lng: 9.190318};
+  coordelephant={lat: 45.506978, lng: 9.191479};
+  coordcow={lat: 45.506252, lng: 9.189657};
 
   constructor()
   {
@@ -24,6 +33,36 @@ export class AppComponent {
     this.position = this.center;
     this.label = "ciao";
     this.circleOptions = this.colour
+
+    let iconData : google.maps.Icon = {
+      url:'./assets/img/cat_acrobat.ico',
+      scaledSize : new google.maps.Size(60,60)
+    }
+  
+    this.markerOptions = {icon:iconData}
+
+
+
+    let iconDatabear : google.maps.Icon = {
+      url:'./assets/img/icons8-bear-16.png',
+      scaledSize : new google.maps.Size(60,60)
+    }
+  
+    this.markerOptionsbear = {icon:iconDatabear}
+
+    let iconDataelephant : google.maps.Icon = {
+      url:'./assets/img/icons8-elephant-16.png',
+      scaledSize : new google.maps.Size(60,60)
+    }
+  
+    this.markerOptionselephant = {icon:iconDataelephant}
+
+    let iconDatacow : google.maps.Icon = {
+      url:'./assets/img/icons8-cow-16.png',
+      scaledSize : new google.maps.Size(60,60)
+    }
+  
+    this.markerOptionscow = {icon:iconDatacow}
   }
   
   red() : void
